47c43b9ea2341628a7074d8400a98fc673e6be2c
MimosaIT <77324155+MimosaIT@users.noreply.github.com>
2021-01-19 00:46:29 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+print("put some skill:")
+unfamiliar_skill = input(">")
+print(f"filltering out:{unfamiliar_skill}")
+
+
+def find_jobs():
+    html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=").text
+    soup = BeautifulSoup(html_text, "lxml")
+    jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
+    for index, job in enumerate(jobs):
+        published_date = job.find("span", class_="sim-posted").span.text
+        if "few" in published_date:
+            company_name = job.find("h3", class_="joblist-comp-name").text.replace(" ", "")
+            skills = job.find("span", class_="srp-skills").text.replace(" ", "")
+            more_info = job.header.h2.a["href"]
+            if unfamiliar_skill not in skills:
+                with open(f"posts/{index}.txt", "w") as f:
+                    f.write(f"company Name: {company_name.strip()}")
+                    f.write(f"skills: {skills.strip()}")
+                    f.write(f"More info: {more_info}")
+                print(f"file saved:{index}")
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f"waitting {time_wait} minutes...")
+        time.sleep(time_wait * 60)
+
